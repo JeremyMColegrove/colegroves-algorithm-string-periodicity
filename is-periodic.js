40f8acec81cade9c856d89fab7aa43f2da1d1f97
Jeremy Colegrove <jeremy@Jeremys-MacBook-Pro.local>
2025-12-03 07:55:32 -0700

    changed variable name

--- is-periodic.js
+++ is-periodic.js
@@ -4,22 +4,22 @@
  * The algorithm is close to O(N) time, providing simple straightforward steps to testing the string
  * Method:
  * 1) Create frequency map of characters in string. "addaadda" -> {a: 4, d: 4}
- * 2) Compute the greatest common multiple of the frequency values. Example:
- *          gcm({g: 5, d: 10}) = 5
- *          gcm({z:4, m: 1}) = 1
- * 3) If gcm is 1 or 0, then return false, it is not a made of repeating substrings
+ * 2) Compute the greatest common divisor of the frequency values. Example:
+ *          gcd({g: 5, d: 10}) = 5
+ *          gcd({z:4, m: 1}) = 1
+ * 3) If gcd is 1 or 0, then return false, it is not a made of repeating substrings
  *
  * If the string S is the substring p repeating k times, then it can be written as S = p^k.
- *  Given the definition of GCM, k must also be a multiple of GCM.
+ *  Given the definition of gcd, k must also be a multiple of gcd.
  * Since every multiple could be the length of the substring, we must test each one.
  * Looping from 2->gm will gaurantee the longest substring, gm->2 will gaurantee the shortest.
- * 4) Loop over every multiple m of GCM, check if substring(0, len(p) / m) is repeating
+ * 4) Loop over every multiple m of gcd, check if substring(0, len(p) / m) is repeating
  *
  *
  * EXAMPLE: "aaabbbaaabbbaaabbb" ("aaabbb" repeating 3 times)
  * 1) Generate frequency map => {a: 9, b: 9}
- * 2) Get GCM => 9
- * 3) GCM is not 0 or 1, continue
+ * 2) Get gcd => 9
+ * 3) gcd is not 0 or 1, continue
  * 4) 3 is the first multiple of 9, so divide original string by 3 and test substring (0, 18/3) => "aaabbb"
  * This is the correct result
  *
@@ -34,18 +34,18 @@ export function isPeriodic(str) {
 	// build map of character frequency O(n)
 	const freq = new Map();
 	for (const ch of str) freq.set(ch, (freq.get(ch) || 0) + 1);
-	// get array of frequency values for gcm
+	// get array of frequency values for gcd
 	const vals = Array.from(freq.values());
-	// find gcm among values O(log(min(a, b)))
+	// find gcd among values O(log(min(a, b)))
 	const gm =
 		vals.length === 0
 			? 0
 			: vals.length === 1
 				? vals[0]
-				: vals.reduce((a, c) => gcm(a, c));
-	// early escape if gcm is 1 or 0
+				: vals.reduce((a, c) => gcd(a, c));
+	// early escape if gcd is 1 or 0
 	if (gm <= 1) return false;
-	// loop over all multiples of gcm
+	// loop over all multiples of gcd
 	for (let k = 2; k <= gm; k++) {
 		if (gm % k !== 0) continue;
 		// test str split into k pieces
@@ -64,10 +64,10 @@ function isRepeating(str, sub) {
 	return true;
 }
 
-function gcm(a, b) {
+function gcd(a, b) {
 	if (b === 0) {
 		return a;
 	}
 
-	return gcm(b, a % b);
+	return gcd(b, a % b);
 }
